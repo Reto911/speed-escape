e6c349e22ebebab263aae0dca43271a1891734e8
Shiroan <shiroan@qq.com>
2024-01-10 12:15:32 +0800

    feat: add quick start cleanup

---
+++ .github/scripts/clean.py
@@ -0,0 +1,46 @@
+import requests
+import sys
+
+def is_quick_start(version) -> bool:
+    for tag in version['metadata']['container']['tags']:
+        if tag.startswith("quick_start_"):
+            return True
+    
+    return False
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print("Please input your token.")
+        exit(-1)
+    
+    token = sys.argv[1]
+
+    headers = {
+        "Accept": "application/vnd.github+json",
+        "Authorization": f"Bearer {token}",
+        "X-GitHub-Api-Version": "2022-11-28",
+    }
+    
+
+    url = "https://api.github.com/users/reto911/packages/container/escape_speed/versions"
+
+    r = requests.get(url=url, headers=headers)
+    
+    code = r.status_code
+    if code != 200:
+        print(f"{code}:", r.text)
+        exit(-1)
+
+    json = r.json()
+
+    package_urls = list(map(lambda v: v['url'], filter(is_quick_start, json)))
+    print(f"{len(package_urls)} packages found:")
+    for package_url in package_urls:
+        print(package_url)
+
+    for package_url in package_urls:
+        r = requests.delete(url=package_url, headers=headers)
+        code = r.status_code
+        if code != 204:
+            print(f"{code}:", r.text)
+            exit(-1)
